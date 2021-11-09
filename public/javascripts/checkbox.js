e0a7fc88c3fd585267212600cc5eb6e6e6f0cd30
Josh <cerebral_hhh@hotmail.com>
2021-11-09 11:48:20 +0800

    aboutUs

--- public/javascripts/checkbox.js
+++ public/javascripts/checkbox.js
@@ -1,4 +1,4 @@
-// the number '3' in line 9 in here matches the number of properties we want to search in the searchbar in the index page. there should be 5 but one is general search (that uses text index from mongo) for location or name or propertyType. So there are other 4 fields to loop over.
+// the number '2' in line 6 in here matches the number of properties we want to search in the searchbar in the index page. there should be 5 but one is general search (that uses text index from mongo) for location or name or propertyType. So there are other 4 fields to loop over.
 document.getElementById("checkbox").onchange = function() {
     document.getElementById("inputField").disabled=!this.checked
     }
